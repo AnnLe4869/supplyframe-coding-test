f8217a20f0c31e9912be52742f00a44ab551e0f6
Anh Le <kunquan2345@gmail.com>
2022-02-20 08:06:38 -0800

    Make the pagination workable
    - Copy from my old repo that has pagination working
      Old repo: https://github.com/AnnLe4869/camp-view

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import getRankingAnimes from "./helper/getRankingAnimes.js";
-import { NUMBER_OF_ANIMES_TO_DISPLAY, STARTING_RANK } from "./constants.js";
+import { NUMBER_OF_ANIMES_TO_DISPLAY, STARTING_RANK, TOTAL_NUMBER_OF_ANIMES_EXISTING } from "./constants.js";
 
 const app = express();
 const PORT = 5000;
@@ -9,15 +9,22 @@ app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
   try {
+    // the root route and page == 1 is the same
+    // all other page remain the same
     const page = req.query.page ? Math.ceil(req.query.page) : STARTING_RANK
     if(page < 0){
       res.status(500)
       res.render("error")
     }
     
-    const data  = await getRankingAnimes(NUMBER_OF_ANIMES_TO_DISPLAY, NUMBER_OF_ANIMES_TO_DISPLAY * page)
+    // we have to do page - 1 because our page starting from 1 and our ranking starting from 0
+    // for example, page 3 will show ranks from 21 to 30
+    const data  = await getRankingAnimes(NUMBER_OF_ANIMES_TO_DISPLAY, NUMBER_OF_ANIMES_TO_DISPLAY * (page - 1))
+    
     res.render("index.ejs", {
-      animes: data
+      animes: data,
+      currentPage: page,
+      totalPages: Math.ceil(TOTAL_NUMBER_OF_ANIMES_EXISTING / NUMBER_OF_ANIMES_TO_DISPLAY)
     });
   } catch (error) {
     console.log(error);
--- constants.js
+++ constants.js
@@ -1,2 +1,7 @@
 export const NUMBER_OF_ANIMES_TO_DISPLAY = 10
-export const STARTING_RANK = 0
+export const STARTING_RANK = 1
+
+// This is an estimation of how many animes existing in MAL database
+// I cannot confirm at the moment due to the fact that there is no API from MAL that list how many animes they have
+// The actual number may be a lot bigger
+export const TOTAL_NUMBER_OF_ANIMES_EXISTING = 3000
